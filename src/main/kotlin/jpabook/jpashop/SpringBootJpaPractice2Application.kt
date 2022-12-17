a38cfe190d85534597137b7267b6fca0c2088d38
wafflejuice <wafflejuice96@gmail.com>
2022-12-17 23:47:19 +0900

    Initial commit

---
+++ src/main/kotlin/jpabook/jpashop/SpringBootJpaPractice2Application.kt
@@ -0,0 +1,11 @@
+package jpabook.jpashop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootJpaPractice2Application
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootJpaPractice2Application>(*args)
+}
