ab37450f20095cede8be8b8727254b60f59019f2
wafflejuice <wafflejuice96@gmail.com>
2023-09-01 23:58:37 +0900

    주문 조회 V2: 엔티티를 DTO로 변환 #1

--- src/main/kotlin/jpabook/jpashop/api/OrderApiController.kt
+++ src/main/kotlin/jpabook/jpashop/api/OrderApiController.kt
@@ -1,10 +1,14 @@
 package jpabook.jpashop.api
 
+import jpabook.jpashop.domain.Address
 import jpabook.jpashop.domain.Order
+import jpabook.jpashop.domain.OrderItem
+import jpabook.jpashop.domain.OrderStatus
 import jpabook.jpashop.repository.OrderRepository
 import jpabook.jpashop.repository.OrderSearch
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
 
 @RestController
 class OrderApiController(
@@ -23,5 +27,33 @@ class OrderApiController(
         }
 
         return all
+    }
+
+    @GetMapping("/api/v2/orders")
+    fun ordersV2(): List<OrderDto> {
+        val orders = orderRepository.findAllByString(OrderSearch())
+        val collect = orders.map { OrderDto.of(it) }
+        return collect
+    }
+
+    data class OrderDto(
+        val orderId: Long,
+        val name: String,
+        val orderDate: LocalDateTime,
+        val orderStatus: OrderStatus,
+        val address: Address?,
+        val orderItems: List<OrderItem>,
+    ) {
+        companion object {
+            fun of(order: Order) =
+                OrderDto(
+                    orderId = order.id,
+                    name = order.member.name,
+                    orderDate = order.orderDate,
+                    orderStatus = order.status,
+                    address = order.delivery.address,
+                    orderItems = order.orderItems,
+                )
+        }
     }
 }
